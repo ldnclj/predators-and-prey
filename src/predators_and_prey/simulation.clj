1efe72bcb49db33b55ed50347b0cb88a85e96ef7
Robert Rees <robert@wazoku.com>
2011-06-13 16:23:14 +0100

    Switching to vector velocity

--- src/predators_and_prey/simulation.clj
+++ src/predators_and_prey/simulation.clj
@@ -1,23 +1,24 @@
 (ns predators-and-prey.simulation)
 
-(def predator {:velocity 2 :max-velocity 7 :radius 10})
-(def prey {:velocity 2 :max-velocity 4 :radius 5})
+(def predator {:vx 2 :vy 2 :max-velocity 7 :radius 10})
+(def prey {:vx 2 :vy 2 :max-velocity 4 :radius 5})
 
 (def animals (atom {}))
 
-(defn create-predator [x y heading]
-	(conj {:x x :y y :heading heading} predator))
+(defn create-predator [x y horizontal-velocity vertical-velocity]
+	(conj {:x x :y y :vx horizontal-velocity :vy vertical-velocity} predator))
+	
 (defn create-prey
 	([screen-size]
-	(conj {:x (rand-int screen-size) :y (rand-int screen-size) :heading (rand-int 360)} prey))
-	([x y heading]
-	(conj {:x x :y y :heading heading} prey)))
+	(conj {:x (rand-int screen-size) :y (rand-int screen-size) :vx (rand-int (:max-velocity prey)) :vy (rand-int (:max-velocity prey))} prey))
+	([x y vx vy]
+	(conj {:x x :y y :vx vx :vy vy} prey)))
 	
 (defn prey-generator [screen-size]
-	#(conj {:x (rand-int screen-size) :y (rand-int screen-size) :heading (rand-int 360)} prey))
+	#(conj {:x (rand-int screen-size) :y (rand-int screen-size) ::vx (rand-int (:max-velocity prey)) :vy (rand-int (:max-velocity prey))} prey))
 	
 (defn initial-state [screen-size]
-	{:predators [(create-predator 50 50 200) (create-predator (- screen-size 100) (- screen-size 100) 200)]
+	{:predators [(create-predator 50 50 4 4) (create-predator (- screen-size 100) (- screen-size 100) -4 -4)]
 	:prey (take 50 (repeatedly (prey-generator screen-size)))})
 
 (defn think [screen-size]
