fdc2a2d0ee70febb9a455c11437abfba6fe8d6c7
Robert Rees <robert@wazoku.com>
2011-06-13 16:01:51 +0100

    Adding a thinking slot for the AI code to slot into

--- src/predators_and_prey/simulation.clj
+++ src/predators_and_prey/simulation.clj
@@ -17,10 +17,15 @@
 	#(conj {:x (rand-int screen-size) :y (rand-int screen-size) :heading (rand-int 360)} prey))
 	
 (defn initial-state [screen-size]
-	{:predators [(create-predator 50 50 200) (create-predator 80 50 200)]
+	{:predators [(create-predator 50 50 200) (create-predator (- screen-size 100) (- screen-size 100) 200)]
 	:prey (take 50 (repeatedly (prey-generator screen-size)))})
+
+(defn think [screen-size]
+	@animals)
 
 (defn pulse [screen-size]
 	(let [bounded-screen-size (- screen-size 20)]
-	(if (empty? @animals) (swap! animals conj (initial-state screen-size)))
-	@animals))
+	(if (empty? @animals)
+		(swap! animals conj (initial-state screen-size)
+		(swap! animals conj (think screen-size)))))
+	@animals)
