4242cdf8a2fe41aee621a644ee2698f1d3614e28
Bruce Durling <bld@otfrom.com>
2011-06-27 17:51:35 +0100

    Updated swank-clojure to latest non-snapshot release.

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
 (defproject predators-and-prey "0.0.1-SNAPSHOT"
-		:description "Predators and Prey example"
-		:dependencies [[org.clojure/clojure "1.2.0"]
-		[org.clojure/clojure-contrib "1.2.0"]
-		[midje "1.1.1"]
-		[org.clojars.automata/rosado.processing "1.1.0"]]
-
-                :dev-dependencies [ [swank-clojure "1.2.1"]]
-
-                :main predators-and-prey.core
-
-                )
+  :description "Predators and Prey example"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [midje "1.1.1"]
+                 [org.clojars.automata/rosado.processing "1.1.0"]]
+  :dev-dependencies [ [swank-clojure "1.3.1"]]
+  :main predators-and-prey.core)
